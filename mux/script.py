141208bbe8cca7fae479288d602f7ab8a773b6a5
Jonathan Cagua <jonathan.cagua@gmail.com>
2022-03-20 00:37:54 -0500

    ADD:mux

---
+++ mux/script.py
@@ -0,0 +1,10 @@
+from os import system
+vhdl_file = 'mux'
+vhdl_file_tb = vhdl_file+'_tb'
+line_vhdl = vhdl_file+'.vhd ' + vhdl_file_tb+'.vhd'
+
+system("ghdl -a "+line_vhdl)
+system("ghdl -s "+line_vhdl)
+system("ghdl -e "+vhdl_file_tb)
+system("ghdl -r "+vhdl_file_tb+" --vcd="+ vhdl_file_tb+".vcd --stop-time=1000ns")
+system("open /Applications/gtkwave.app "+ vhdl_file_tb+".vcd")
